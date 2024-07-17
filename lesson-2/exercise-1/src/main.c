ef090bf83c6af26d16cd849ad0f534c2e82f8a20
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2024-07-17 09:42:39 +0200

    Format the code using the project .clang-format file.

--- lesson-2/exercise-1/src/main.c
+++ lesson-2/exercise-1/src/main.c
@@ -9,7 +9,6 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 
-
 /* STEP 7 - Change the sleep time from 1000 ms to 100 ms */
 #define SLEEP_TIME_MS 100
 
@@ -24,37 +23,38 @@ static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 #define LED0_NODE DT_ALIAS(led0)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-int main(void) {
-  int ret;
+int
+main(void) {
+    int ret;
 
-  if (!device_is_ready(led.port)) {
-    return -1;
-  }
-  /* STEP 4 - Verify that the device is ready for use */
-  if (!device_is_ready(button.port)) {
-    return -1;
-  }
+    if (!device_is_ready(led.port)) {
+        return -1;
+    }
+    /* STEP 4 - Verify that the device is ready for use */
+    if (!device_is_ready(button.port)) {
+        return -1;
+    }
 
-  ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-  if (ret < 0) {
-    return -1;
-  }
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        return -1;
+    }
 
-  /* STEP 5 - Configure the pin connected to the button to be an input pin and
-   * set its hardware specifications */
-   ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-if (ret < 0) {
-	return -1;
-}
+    /* STEP 5 - Configure the pin connected to the button to be an input pin and
+     * set its hardware specifications */
+    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+    if (ret < 0) {
+        return -1;
+    }
 
-  while (1) {
-    /* STEP 6.1 - Read the status of the button and store it */
-    bool val = gpio_pin_get_dt(&button);
+    while (1) {
+        /* STEP 6.1 - Read the status of the button and store it */
+        bool val = gpio_pin_get_dt(&button);
 
-    /* STEP 6.2 - Update the LED to the status of the button */
-     gpio_pin_set_dt(&led,val);
+        /* STEP 6.2 - Update the LED to the status of the button */
+        gpio_pin_set_dt(&led, val);
 
-    k_msleep(SLEEP_TIME_MS);  // Put the main thread to sleep for 100ms for
-                              // power optimization
-  }
+        k_msleep(SLEEP_TIME_MS);    // Put the main thread to sleep for 100ms
+                                    // for power optimization
+    }
 }
