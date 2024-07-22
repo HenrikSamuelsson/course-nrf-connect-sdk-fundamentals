51c30805e334999dab72b0e9ed30be121ebda384
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2024-07-22 06:46:12 +0200

    Add Kconfig to configure what goes into the project.

--- lesson-3/exercise-2/src/main.c
+++ lesson-3/exercise-2/src/main.c
@@ -1,14 +1,21 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
-
+#ifdef CONFIG_MYFUNCTION
 #include "myfunction.h"
+#endif
 
-int main(void) {
-    int a = 3, b = 4;
+void main(void) {
+
     while (1) {
+#ifdef CONFIG_MYFUNCTION
+        int a = 3, b = 4;
         printk("The sum of %d and %d is %d\n\r", a, b, sum(a, b));
         a++;
         b++;
+#else
+        printk("MYFUNCTION not enabled\r\n");
+        return;
+#endif
         k_msleep(1000);
     }
 }
